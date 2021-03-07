a71144b08c72c9e13a1508c39e611f1f976f2c03
John Alexander Florez <78865261+Jhon0216@users.noreply.github.com>
2021-03-07 14:44:25 -0500

    Ejercicio #3
    Ejercicio #3 de Taller JavaScript #1

---
+++ EJERCICIO #3/Scripts/javascript.js
@@ -0,0 +1,111 @@
+var n1 = prompt('Escriba un numero');
+var n2 = prompt('Escriba otro numero');
+var n3 = prompt('Escriba otro numero');
+var n4 = prompt('Escriba otro numero');
+var n5 = prompt('Escriba otro numero');
+var n6 = prompt('Escriba otro numero');
+var n7 = prompt('Escriba otro numero');
+var n8 = prompt('Escriba otro numero');
+var n9 = prompt('Escriba otro numero');
+var n10 = prompt('Escriba otro numero');
+
+if(n1<0) {
+	document.write('<li>');
+	document.write('El numero ' + n1 + ' es menor a cero');
+	document.write('</li>');
+} else{
+	document.write('<li>');
+	document.write('El numero ' + n1 + ' es mayor a cero');
+	document.write('</li>');
+}
+
+
+if(n2<0) {
+	document.write('<li>');
+	document.write('El numero ' + n2 + ' es menor a cero');
+	document.write('</li>');
+} else{
+	document.write('<li>');
+	document.write('El numero ' + n2 + ' es mayor a cero');
+	document.write('</li>');
+}
+
+if(n3<0) {
+	document.write('<li>');
+	document.write('El numero ' + n3 + ' es menor a cero');
+	document.write('</li>');
+} else{
+	document.write('<li>');
+	document.write('El numero ' + n3 + ' es mayor a cero');
+	document.write('</li>');
+}
+
+if(n4<0) {
+	document.write('<li>');
+	document.write('El numero ' + n4 + ' es menor a cero');
+	document.write('</li>');
+} else{
+	document.write('<li>');
+	document.write('El numero ' + n4 + ' es mayor a cero');
+	document.write('</li>');
+}
+
+if(n5<0) {
+	document.write('<li>');
+	document.write('El numero ' + n5 + ' es menor a cero');
+	document.write('</li>');
+} else{
+	document.write('<li>');
+	document.write('El numero ' + n5 + ' es mayor a cero');
+	document.write('</li>');
+}
+
+if(n6<0) {
+	document.write('<li>');
+	document.write('El numero ' + n6 + ' es menor a cero');
+	document.write('</li>');
+} else{
+	document.write('<li>');
+	document.write('El numero ' + n6 + ' es mayor a cero');
+	document.write('</li>');
+}
+
+if(n7<0) {
+	document.write('<li>');
+	document.write('El numero ' + n7 + ' es menor a cero');
+	document.write('</li>');
+} else{
+	document.write('<li>');
+	document.write('El numero ' + n7 + ' es mayor a cero');
+	document.write('</li>');
+}
+
+if(n8<0) {
+	document.write('<li>');
+	document.write('El numero ' + n8 + ' es menor a cero');
+	document.write('</li>');
+} else{
+	document.write('<li>');
+	document.write('El numero ' + n8 + ' es mayor a cero');
+	document.write('</li>');
+}
+
+if(n9<0) {
+	document.write('<li>');
+	document.write('El numero ' + n9 + ' es menor a cero');
+	document.write('</li>');
+} else{
+	document.write('<li>');
+	document.write('El numero ' + n9 + ' es mayor a cero');
+	document.write('</li>');
+}
+
+if(n10<0) {
+	document.write('<li>');
+	document.write('El numero ' + n10 + ' es menor a cero');
+	document.write('</li>');
+} else{
+	document.write('<li>');
+	document.write('El numero ' + n10 + ' es mayor a cero');
+	document.write('</li>');
+}
