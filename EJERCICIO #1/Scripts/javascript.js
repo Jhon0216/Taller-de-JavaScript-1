e8b6d4a081458cd9659d1b9fa364679d6586be0d
John Alexander Florez <78865261+Jhon0216@users.noreply.github.com>
2021-03-07 14:40:49 -0500

    Ejercicio #1
    Ejercicio #1 de Taller JavaScript #1

---
+++ EJERCICIO #1/Scripts/javascript.js
@@ -0,0 +1,3 @@
+var d=new Date();
+window.alert("Fecha" + ': '+ d.getDate() + '-' + (d.getMonth() + 1) + '-' + d.getFullYear() + '  ' +
+             "Hora"+ ': ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds());
